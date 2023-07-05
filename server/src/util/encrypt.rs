e96783692d9ed8e1492e3d7fff91ed743e414f64
yangbiao <yangbiao@makeblock.com>
2023-07-05 23:48:59 +0800

    feat: Encrypted

--- server/src/util/encrypt.rs
+++ server/src/util/encrypt.rs
@@ -1,6 +1,7 @@
 use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
-use rsa::pkcs8::{EncodePrivateKey, EncodePublicKey, LineEnding};
+use rsa::pkcs8::{DecodePrivateKey, DecodePublicKey, EncodePrivateKey, EncodePublicKey, LineEnding};
 use base64::{decode, encode};
+use rand::prelude::ThreadRng;
 
 const KEY_SIZE: usize = 1024;
 
@@ -8,6 +9,35 @@ pub fn generate_keys() -> (RsaPrivateKey, RsaPublicKey) {
     let mut rng = rand::thread_rng();
     let priv_key = RsaPrivateKey::new(&mut rng, KEY_SIZE).expect("failed to generate a key");
     let pub_key = RsaPublicKey::from(&priv_key);
+    (priv_key, pub_key)
+}
+
+pub fn default_keys() -> (RsaPrivateKey, RsaPublicKey) {
+    let pub_key_pem = "-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCxC6org1ozXX9i+FS+eLwM0+kv
+00ABH0Nf4TC0Ruisw86gi/tLgTaMFC2kH58aTCAXitXbRnY8TVVxKpo3n193Qami
+Fpdv9W3pWX7VgLVxcQ+fG1j8mSDUX1IcsIAHU+ugt51i4/CMc8r43oEAvoHe1Nc7
+9JqHpiPMjC9n5jkVewIDAQAB
+-----END PUBLIC KEY-----";
+    let priv_key_pem = "-----BEGIN PRIVATE KEY-----
+MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBALELqiuDWjNdf2L4
+VL54vAzT6S/TQAEfQ1/hMLRG6KzDzqCL+0uBNowULaQfnxpMIBeK1dtGdjxNVXEq
+mjefX3dBqaIWl2/1belZftWAtXFxD58bWPyZINRfUhywgAdT66C3nWLj8Ixzyvje
+gQC+gd7U1zv0moemI8yML2fmORV7AgMBAAECgYEAkwbl3dbqXIfD11P1RGyNdnWx
+TEc5+vSUhhZD1SKh7X96o/c7zHtU6kWDla2w+izH5eUlLkE4xWFPmHhRs+mFY+iR
+QkRsdS5+AZ0o/xZoOzN+Wk+m7yH8iWJA/I8HIKnUVci76Yjf/+PKDhfXxfbFDaWz
+msnfWBqiM2Qu2xGmtkECQQDEydSq0rtE28DeiKH4T1lFTGFMlmsyOspFPTwU1wR3
+JdVCX04LSJUyYcsy8vTKK6Wf730jE8ELGklFsjGWYvihAkEA5lEWbc07FuFz9Jel
+XGdFrkT9Yq516LkL7/QKpSdeYgUHWG3PDubQOGdK8lKulA0zcOjQxmf/3TZeCyjw
+aYUMmwJBALAy5zBsrzDgcrjOLfhfU9cwpI4mwKSg7ys/wOQIAf+M6H4dR4vi2XPb
+DE36LCQ5IHsrvCgEL/z49FJrUusMgwECQH3Zwzq+kVJAYZ8+mgIzADVox1h74jwH
+mYlFvede6sj+3HOqlSEcaJzZfG+LVI/b4scIwMQZogtboOnSNgRCyo8CQBd8PS6F
+VcJC60FS26OgHgAixikmIsdR5JCSO3PmLVtRbGsHmIZm9NR4YV8U/X3NRw+ZBQ1Z
+27pOPx4/O7yT3yY=
+-----END PRIVATE KEY-----";
+    //从上面的字符串读取公钥和私钥
+    let pub_key = RsaPublicKey::from_public_key_pem(pub_key_pem).unwrap();
+    let priv_key: RsaPrivateKey =  DecodePrivateKey::from_pkcs8_pem(priv_key_pem).unwrap();
     (priv_key, pub_key)
 }
 
@@ -19,16 +49,17 @@ pub fn fmt_print_keys(pub_key: &RsaPublicKey, priv_key: &RsaPrivateKey) -> (Stri
     (pub_key_pem.as_str().to_string(), priv_key_pem.as_str().to_string())
 }
 
-
-pub fn encrypt(pub_key: &RsaPublicKey, data: &[u8]) -> String {
+// 加密
+pub fn encrypt(pub_key: &RsaPublicKey, ciphertext: &[u8]) -> String {
     let mut rng = rand::thread_rng();
-    let enc_data = pub_key.encrypt(&mut rng, Pkcs1v15Encrypt, &data[..]).expect("failed to encrypt");
+    let enc_data = pub_key.encrypt(&mut rng, Pkcs1v15Encrypt, &ciphertext[..]).expect("failed to encrypt");
     encode(&enc_data)
 }
 
-pub fn decrypt(priv_key: &RsaPrivateKey, enc_data: &str) -> String {
-    let dec_data = priv_key.decrypt(Pkcs1v15Encrypt, enc_data.as_bytes()).expect("failed to decrypt");
-     std::str::from_utf8(&dec_data).unwrap().to_string()
+// 解密
+pub fn decrypt(priv_key: &RsaPrivateKey, ciphertext: &[u8]) -> String {
+    let dec_data = priv_key.decrypt(Pkcs1v15Encrypt, ciphertext).expect("failed to decrypt");
+    std::str::from_utf8(&dec_data).unwrap().to_string()
 }
 
 #[cfg(test)]
@@ -102,12 +133,15 @@ VcJC60FS26OgHgAixikmIsdR5JCSO3PmLVtRbGsHmIZm9NR4YV8U/X3NRw+ZBQ1Z
         let mut rng = rand::thread_rng();
         let data = b"abcdd@/_efafdcas2dsa323";
         let enc_data = pub_key.encrypt(&mut rng, Pkcs1v15Encrypt, &data[..]).expect("failed to encrypt");
-        println!("Encrypted data: {}", encode(&enc_data));
+        let str = encode(&enc_data);
+        println!("Encrypted data: {}", str);
         assert_ne!(&data[..], &enc_data[..]);
         // Decrypt
+        // let dec_data = priv_key.decrypt(Pkcs1v15Encrypt, &enc_data).expect("failed to decrypt");
+        let enc_data = decode(&str).expect("failed to decode Base64");
         let dec_data = priv_key.decrypt(Pkcs1v15Encrypt, &enc_data).expect("failed to decrypt");
         assert_eq!(&data[..], &dec_data[..]);
-        println!("Decrypted data: {:?}", enc_data);
+        println!("Decrypted data: {:?}", std::str::from_utf8(&dec_data).unwrap());
     }
 
     #[test]
@@ -136,19 +170,13 @@ VcJC60FS26OgHgAixikmIsdR5JCSO3PmLVtRbGsHmIZm9NR4YV8U/X3NRw+ZBQ1Z
 -----END PRIVATE KEY-----";
         //从上面的字符串读取公钥和私钥
         let pub_key = RsaPublicKey::from_public_key_pem(pub_key_pem).unwrap();
-        // let priv_key = RsaPrivateKey::from_public_key_pem(priv_key_pem).unwrap();
         let priv_key: RsaPrivateKey =  DecodePrivateKey::from_pkcs8_pem(priv_key_pem).unwrap();
-
-        let mut rng = rand::thread_rng();
         let data = b"abcdd@/_efafdcas2dsa323";
-        // let enc_data = pub_key.encrypt(&mut rng, Pkcs1v15Encrypt, &data[..]).expect("failed to encrypt");
-        let enc_data = encrypt(&pub_key,data);
-        println!("Encrypted data: {}",enc_data);
-        // assert_ne!(&data[..], &enc_data[..]);
-        // Decrypt
-        // let dec_data = priv_key.decrypt(Pkcs1v15Encrypt, &enc_data).expect("failed to decrypt");
+        let mut rng = rand::thread_rng();
+        let enc_data_str = encrypt(&pub_key,data);
+        println!("Encrypted data: {}",enc_data_str);
+        let enc_data = decode(&enc_data_str).expect("failed to decode Base64");
         let dec_data = decrypt(&priv_key,&enc_data);
-        // assert_eq!(&data[..], &dec_data[..]);
-        println!("Decrypted data: {:?}", enc_data);
+        println!("Decrypted data: {:?}", dec_data);
     }
 }
